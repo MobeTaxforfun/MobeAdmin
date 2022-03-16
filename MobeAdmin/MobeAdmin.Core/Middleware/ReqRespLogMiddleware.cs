af23f2e429e1efbba5da38ef3caff72cb71355d0
Paulwu <paulwu@gis.tw>
2022-03-16 11:00:18 +0800

    新增 ReqRespLogMiddleware 雛形( 之後加 Attribute 判斷與 Serilog)

--- MobeAdmin/MobeAdmin.Core/Middleware/ReqRespLogMiddleware.cs
+++ MobeAdmin/MobeAdmin.Core/Middleware/ReqRespLogMiddleware.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,14 +20,41 @@ namespace MobeAdmin.Core.Middleware
         public async Task InvokeAsync(HttpContext context)
         {
             context.Request.EnableBuffering();
+            Stream originalBody = context.Response.Body;
+            try
+            {
+                await LogRequest(context);
+
+                using (var ms = new MemoryStream())
+                {
+                    context.Response.Body = ms;
+
+                    await _next(context);
+
+                    LogResponse(context.Response, ms);
+
+                    ms.Position = 0;
+                    await ms.CopyToAsync(originalBody);
+                }
+            }
+            catch (Exception ex)
+            {
+                                
+            }
+            finally
+            {
+                context.Response.Body = originalBody;
+            }
         }
 
-        private void LogRequest(HttpContext context)
-        { 
-        
+        private async Task LogRequest(HttpContext context)
+        {
+            var request = context.Request;
+            var sr = new StreamReader(request.Body);
+            var body = await sr.ReadToEndAsync();
         }
 
-        private void LogResponse(HttpContext context)
+        private void LogResponse(HttpResponse context, MemoryStream ms)
         {
 
         }
