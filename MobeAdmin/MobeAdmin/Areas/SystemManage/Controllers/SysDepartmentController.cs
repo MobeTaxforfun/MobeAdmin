0c6e4b4f494c587ef344aa2b1c27d78d083b35f4
MobeWu <mobetax81072@gmail.com>
2022-03-17 00:11:00 +0800

    更新 拉一下介面 + 一個基礎的 Controller

---
+++ MobeAdmin/MobeAdmin/Areas/SystemManage/Controllers/SysDepartmentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobeAdmin.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobeAdmin.Areas.SystemManage.Controllers
+{
+    [Area("SystemManage")]
+    public class SysDepartmentController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MobeAdmin/MobeAdmin/Controllers/BaseController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobeAdmin.Domain.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobeAdmin.Controllers
+{
+    public class BaseController : Controller
+    {
+        /// <summary>
+        /// 回應成功(有物件)
+        /// </summary>
+        /// <typeparam name="T">型態</typeparam>
+        /// <param name="data">回傳結果</param>
+        /// <param name="message">提示</param>
+        /// <returns></returns>
+        [NonAction]
+        public BaseMessageAPI<T> Success<T>(T data, string message = "操作成功")
+        {
+            return new BaseMessageAPI<T>()
+            {
+                Success = true,
+                Message = message,
+                Data = data,
+            };
+        }
+
+        /// <summary>
+        /// 回應成功(無物件)
+        /// </summary>
+        /// <param name="message">提示</param>
+        /// <returns></returns>
+        [NonAction]
+        public BaseMessageAPI Success(string message = "操作成功")
+        {
+            return new BaseMessageAPI()
+            {
+                Success = true,
+                Message = message,
+            };
+        }
+
+        /// <summary>
+        /// 回應失敗(無物件)
+        /// </summary>
+        /// <param name="message">提示</param>
+        /// <returns></returns>
+        [NonAction]
+        public BaseMessageAPI<T> Failed<T>(T data, string message = "操作失敗")
+        {
+            return new BaseMessageAPI<T>()
+            {
+                Success = false,
+                Message = message,
+                Data = data
+            };
+        }
+
+        [NonAction]
+        public BaseMessageAPI Failed(string message = "操作失敗")
+        {
+            return new BaseMessageAPI()
+            {
+                Success = false,
+                Message = message,
+            };
+        }
+
+    }
+}
