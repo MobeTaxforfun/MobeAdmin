0b43bc9e0222fb23b1ec25951a8d28204f7925f5
Paulwu <paulwu@gis.tw>
2022-03-16 10:32:37 +0800

    新增 ListedServiceMiddleware,有個東西要測試先Commit一下

---
+++ MobeAdmin/MobeAdmin.Core/Middleware/ReqRespLogMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobeAdmin.Core.Middleware
+{
+    public class ReqRespLogMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ReqRespLogMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+        }
+
+        private void LogRequest(HttpContext context)
+        { 
+        
+        }
+
+        private void LogResponse(HttpContext context)
+        {
+
+        }
+
+    }
+
+}
